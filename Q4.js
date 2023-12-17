2db3e6b8935eb56db7b4f730314b3e1dc335b97c
mohabat <mohabatullahomarzai@gimal.com>
2023-12-16 18:01:58 -0800

    Create an object representing a person with properties like name, age, and address.

---
+++ Q4.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Mohabat',
+    lastname: 'Omarzai',
+    age: 21
+}
+console.log(person.name);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person);
